fa0dfcd3e083bf88d88ccb912c6136d0f4071659
SickMyDucks <me@neilrichter.com>
2018-03-02 17:43:32 +0000

    Can now login and define username in sessionsStorage

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 window.onload = () => {
-    insert = document.querySelector('.insert');
-    ajaxCall('test.html', insert);
+    main = document.querySelector('main');
+    header = document.querySelector('header');
+    if (typeof sessionStorage.username != 'undefined') {
+        display('menu.html', header);
+    } else {
+        display('login.html', main, login);
+    }
 }
 
-function ajaxCall(url, target) {
+function display(url, target, callback) {
     var http = new XMLHttpRequest();
     http.open("GET", url);
     http.onload = () => {
         if (http.readyState == 4 && http.status == 200) {
             target.innerHTML = http.responseText;
+            if (typeof callback != 'undefined') {
+                callback();
+            }
         }
     };
     http.send();
+}
+
+function setUsername(username) {
+    document.querySelector('nav div:nth-child(2)').innerHTML = username;
+}
+
+function login(setUsername) {
+    document.querySelector('form[name="login"]').onsubmit = function() {
+        changeUsername(this[0].value);
+        display('menu.html', header);
+        return false;
+    }
+}
+
+function changeUsername(value) {
+    sessionStorage.setItem('username', value);
 }
